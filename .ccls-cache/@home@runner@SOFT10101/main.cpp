402d10c4fad8245c9f0bd173f1953422ec11b640
baruwaa12 <45532744+baruwaa12@users.noreply.github.com>
2023-01-06 15:57:50 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@SOFT10101/main.cpp
@@ -0,0 +1,23 @@
+// This file contains the 'main' function. Program execution starts and ends
+// here.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 999;
+  double y = 42.5;
+  bool Answer = true;
+
+  // << = LeftShift
+  // >> = rightShift
+  // endl = endline
+  // cout = object of iostream which is used to display things on an output
+  // device NaN = not a number
+
+  cout << "The value of x is " << x << endl;
+  cout << "This is a section of text "
+       << " and this is another" << endl;
+  cout << "The value of y is " << y << endl;
+  cout << "The value of Answer is " << Answer << endl;
+}
