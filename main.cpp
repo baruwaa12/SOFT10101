9f4f6adebea14fa3293770002fa2d7a82d5b1072
baruwaa12 <45532744+baruwaa12@users.noreply.github.com>
2023-01-12 12:05:57 +0000

    Finished Lab 1

--- main.cpp
+++ main.cpp
@@ -15,9 +15,31 @@ int main() {
   // cout = object of iostream which is used to display things on an output
   // device NaN = not a number
 
+
+
+  int yy = 3;
+  int aa = 6;
+  int b = 4;
+  int c = 7;
+  int d = 2;
+  
+  x = yy + (aa + b) * 2 / (c-d);
+
+  float bmi = 0;
+  float weight = 72;
+  float height = 1.86;
+
+  bmi = weight / (height * height);
+
+  
+
   cout << "The value of x is " << x << endl;
   cout << "This is a section of text "
        << " and this is another" << endl;
   cout << "The value of y is " << y << endl;
   cout << "The value of Answer is " << Answer << endl;
+
+  cout << "BMI = " << bmi << endl;
 }
+
+ 
